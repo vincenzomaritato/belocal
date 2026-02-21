9575540ec6cfd2c6ffc8399ba7d43f29e4f788fc
Vincenzo Maritato <vincenzomaritato2005@gmail.com>
2026-02-21 08:58:04 +0100

    Initial commit

---
+++ Waypoint/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Waypoint
+//
+//  Created by Vincenzo Maritato on 21/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Waypoint/WaypointApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaypointApp.swift
+//  Waypoint
+//
+//  Created by Vincenzo Maritato on 21/02/26.
+//
+
+import SwiftUI
+
+@main
+struct WaypointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
